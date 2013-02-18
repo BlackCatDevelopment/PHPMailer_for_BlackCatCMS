9fb70cdc0f848ef22768530bc082ad7b97b4782b
Bianka Martinovic <github.20.cerberus@spamgourmet.com>
2013-02-18 14:43:11 +0100

    initial

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ PHPMailer_for_BlackCatCMS
 =========================
 
 PHPMailer library for use with Black Cat CMS
+
+If you are looking for the original PHPMailer library, please visit
+https://code.google.com/a/apache-extras.org/p/phpmailer/
+
---
+++ info.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or (at
+ *   your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *   General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ *   @author          Black Cat Development
+ *   @copyright       2013, Black Cat Development
+ *   @link            http://blackcat-cms.org
+ *   @license         http://www.gnu.org/licenses/gpl.html
+ *   @category        CAT_Module
+ *   @package         lib_phpmailer
+ *
+ */
+
+$module_directory    = 'lib_phpmailer';
+$module_name         = 'PHPMailer Library';
+$module_function     = 'library';
+$module_version      = '5.2.3';
+$module_platform     = '1.0.x';
+$module_requirements = 'PHP 5.2 or higher';
+$module_author 		 = 'Andy Prevost, Marcus Bointon, Brent R. Matzelle (see class.pop3.php for more details); Black Cat Development (Module)';
+$module_home		 = 'https://code.google.com/a/apache-extras.org/p/phpmailer/';
+$module_license 	 = 'GNU General Public License (module); LGPLv2 (PHPMailer)';
+$module_description  = 'PHPMailer Library for use with Black Cat CMS';
+$module_guid         = '4F10A434-35A5-45C3-9B2C-F718F423E0FD';
+
+?>
